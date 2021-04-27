dfba5eb2ed1b15ff2eb254ac7506a19569ca26dd
semenantufiev <alexantufiev@gmail.com>
2021-04-27 19:36:20 +0300

    Add exception

---
+++ src/main/java/ru/spbstu/antufievsemen/courseClientOracleDB/exception/BookLimitException.java
@@ -0,0 +1,7 @@
+package ru.spbstu.antufievsemen.courseClientOracleDB.exception;
+
+public class BookLimitException extends Exception{
+    public BookLimitException(String message) {
+        super(message);
+    }
+}
